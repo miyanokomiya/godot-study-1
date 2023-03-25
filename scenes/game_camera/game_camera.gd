6cf0e4df9a90fcb64e0f1c02a88115ce400b632f
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-03-25 14:39:14 +0900

    -Section 1: Starting Out

---
+++ scenes/game_camera/game_camera.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+var target_position = Vector2.ZERO
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.make_current()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.acquire_target()
+	self.global_position = self.global_position.lerp(target_position, 1.0 - exp(-delta * 10))
+
+
+func acquire_target():
+	var player_nodes = self.get_tree().get_nodes_in_group("player")
+	if player_nodes.size() > 0:
+		var player = player_nodes[0] as Node2D
+		target_position = player.global_position
