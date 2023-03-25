b3423fa3234515919dda566a55133e03a0608a73
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-03-25 14:12:36 +0900

    -3. Player Movement

---
+++ scenes/player/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 200
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var movement_vector = self.get_movement_vector()
+	var direction = movement_vector.normalized()
+	self.velocity = direction * MAX_SPEED
+	self.move_and_slide()
+
+
+func get_movement_vector() -> Vector2:
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return Vector2(x_movement, y_movement)
