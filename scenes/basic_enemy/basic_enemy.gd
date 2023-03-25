057645f2115281fa95d232f4917e0d604ceff4da
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-03-25 20:44:28 +0900

    -11. Destroying Enemies

---
+++ scenes/basic_enemy/basic_enemy.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 75
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Area2D.area_entered.connect(on_area_entered)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = self.get_direction_to_player()
+	self.velocity = direction * MAX_SPEED
+	self.move_and_slide()
+
+
+func get_direction_to_player() -> Vector2:
+	var player_node = self.get_tree().get_first_node_in_group("player") as Node2D
+	if player_node != null:
+		return (player_node.global_position - self.global_position).normalized()
+	return Vector2.ZERO
+
+
+func on_area_entered(other_area: Area2D):
+	self.queue_free()
