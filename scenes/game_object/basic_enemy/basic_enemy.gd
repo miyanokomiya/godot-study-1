f8d440c335a1a4910d1b4b46eb6cb566dfd12dea
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-03-26 16:41:04 +0900

    -20. Creating an Experience Bar

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -4,10 +4,6 @@ const MAX_SPEED = 40
 
 @onready var health_component: HealthComponent = $HealthComponent
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Area2D.area_entered.connect(on_area_entered)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -22,6 +18,3 @@ func get_direction_to_player() -> Vector2:
 		return (player_node.global_position - self.global_position).normalized()
 	return Vector2.ZERO
 
-
-func on_area_entered(other_area: Area2D):
-	health_component.damage(100)
--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -1,6 +1,12 @@
 extends Node
 
+signal experience_updated(current_experience: float, target_experience: float)
+
+const TARGET_EXPERIENCE_GROWTH = 5
+
 var current_experienct = 0
+var current_level = 1
+var target_experience = 5
 
 
 func _ready():
@@ -8,9 +14,13 @@ func _ready():
 
 
 func increment_experience(number: float):
-	current_experienct += number
-	print(current_experienct)
-
+	current_experienct = min(current_experienct + number, target_experience)
+	experience_updated.emit(current_experienct, target_experience)
+	if current_experienct == target_experience:
+		current_level += 1
+		target_experience += TARGET_EXPERIENCE_GROWTH
+		current_experienct = 0
+		experience_updated.emit(current_experienct, target_experience)
 
 func on_experience_vial_collected(number: float):
 	increment_experience(number)
