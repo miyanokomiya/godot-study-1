0e4301156dd8b5b7d8b1d819ca5206d267be1da7
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-03-28 14:24:39 +0900

    -41. Animating the Wizard

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -3,15 +3,17 @@ extends Node
 const SPAWN_RADIUS = 370
 
 @export var basic_enemy_scene: PackedScene
+@export var wizard_enemy_scene: PackedScene
 @export var arena_time_manager: Node
 
 @onready var timer = $Timer
 
 var base_spawn_time = 0
+var enemy_table = WeightedTable.new()
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	enemy_table.add_item(basic_enemy_scene, 10)
 	base_spawn_time = timer.wait_time
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_dificulty_increased.connect(on_arena_dificulty_increased)
@@ -44,7 +46,8 @@ func on_timer_timeout():
 	if player == null:
 		return
 
-	var enemy = basic_enemy_scene.instantiate() as Node2D
+	var enemy_scene = enemy_table.pick_item()
+	var enemy = enemy_scene.instantiate() as Node2D
 	var entities_layer = self.get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
 	enemy.global_position = get_spawn_position()
@@ -54,3 +57,6 @@ func on_arena_dificulty_increased(arena_dificulty: int):
 	var time_off = (0.1 / 12) * arena_dificulty
 	time_off = min(time_off, 0.7)
 	timer.wait_time = base_spawn_time - time_off
+	
+	if arena_dificulty == 6:
+		enemy_table.add_item(wizard_enemy_scene, 20)
