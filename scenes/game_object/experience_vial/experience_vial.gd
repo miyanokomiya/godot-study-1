600c98eb256ae136306a68557fee90245b9a641b
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-03-28 15:51:08 +0900

    -45. Implementing a Flash on Enemy Hit

--- scenes/game_object/experience_vial/experience_vial.gd
+++ scenes/game_object/experience_vial/experience_vial.gd
@@ -1,11 +1,39 @@
 extends Node2D
 
+@onready var collision_shape_2d = $Area2D/CollisionShape2D
+@onready var sprite_2d = $Sprite2D
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	$Area2D.area_entered.connect(on_area_entered)
 
 
-func on_area_entered(other_area: Area2D):
+func tween_collect(percent: float, start_position: Vector2):
+	var player = self.get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
+	
+	self.global_position = start_position.lerp(player.global_position, percent)
+	var direction_from_start = player.global_position - start_position
+	var target_rotation = direction_from_start.angle() + deg_to_rad(90)
+	self.rotation = lerp_angle(self.rotation, target_rotation, 1 - exp(-2 * get_process_delta_time()))
+
+
+func collect():
 	GameEvents.emit_experience_vial_collected(1)
 	self.queue_free()
+
+
+func disable_collision():
+	collision_shape_2d.disabled = true
+
+
+func on_area_entered(other_area: Area2D):
+	Callable(disable_collision).call_deferred()
+	
+	var tween = create_tween()
+	tween.set_parallel()
+	tween.tween_method(tween_collect.bind(self.global_position), 0.0, 1.0, 0.5).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	tween.tween_property(sprite_2d, "scale", Vector2.ZERO, 0.05).set_delay(0.45)
+	tween.chain()
+	tween.tween_callback(collect)
