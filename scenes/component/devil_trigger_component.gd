782aae8269a45a0686869652a21af447357dc27e
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-04-01 20:24:40 +0900

    feat: Enemies' devil trigger

---
+++ scenes/component/devil_trigger_component.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@onready var trigger_timer = $TriggerTimer
+
+@export var trigger_timeout: int = 30
+@export var velocity_component: Node
+
+func _ready():
+	trigger_timer.timeout.connect(on_trigger_timer_timeout)
+	trigger_timer.wait_time = trigger_timeout
+	trigger_timer.start()
+
+
+func on_trigger_timer_timeout():
+	velocity_component.max_speed *= 2.5
+	velocity_component.acceleration *= 2.5
