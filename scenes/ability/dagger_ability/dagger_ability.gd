dd35f8ceb5490fb0d15ee6485d3467a325a41218
miyanokomiya <tomoya.hstexze24632@gmail.com>
2023-04-11 13:49:09 +0900

    feat: Make daggers orbit friendly

--- scenes/ability/dagger_ability/dagger_ability.gd
+++ scenes/ability/dagger_ability/dagger_ability.gd
@@ -1,22 +1,22 @@
 extends SpellAbility
 
-const MAX_RANGE = 100
+const MAX_RANGE = 110
 
 @onready var hitbox_component = $HitboxComponent
 
-var current_direction = Vector2.ZERO
+var current_distance = 0
 
 
 func play_throw(from: Vector2, target_rotation: float):
 	self.global_position = from
 	self.rotation = target_rotation + PI / 2
 	var tween = create_tween()
-	tween.tween_method(on_tween, Vector2.ZERO, Vector2.RIGHT.rotated(target_rotation) * MAX_RANGE, 0.4)
+	tween.tween_method(on_tween, 0, MAX_RANGE, 0.4)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_callback(self.queue_free)
 
 
-func on_tween(direction: Vector2):
-	var diff = direction - current_direction
+func on_tween(distance: float):
+	var diff = Vector2.RIGHT.rotated(self.rotation - PI / 2) * (distance - current_distance)
 	global_position += diff
-	current_direction += diff
+	current_distance = distance
